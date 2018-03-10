6cd498d0a41708e7f5c4177d29477883f170888e
Jacob Hoke <jacob@threads13.com>
2018-03-10 04:23:39 -0500

    Added calculation functions.  Correctly disallowed multipe decimal points in one string.

--- src/components/App.js
+++ src/components/App.js
@@ -6,22 +6,127 @@ import './App.css';
 // should be able to use one component to render
 // all 10 numbers
 class App extends Component {
-  // construct(props){
-  //   super(props);
-  //   this.state = [];
-  // }
+  constructor(props){
+    super(props);
+    this.state = {
+      displayValue: '0',
+      operator: '',
+      operatorIsSet: false,
+      storedValue: '',
+      isDecimal: false
+    };
+  }
 
+  inputDigit(digit){
+    const { displayValue, operator, operatorIsSet, storedValue, isDecimal } = this.state;
+
+    if (operatorIsSet){
+      this.setState({
+        displayValue: displayValue === '0' ? String(digit) : displayValue + digit,
+        operatorIsSet: false
+      });
+    } else {
+      this.setState ({
+        displayValue: displayValue === '0' ? String(digit) : displayValue + digit
+      });
+    }
+  }
+
+  inputOperator(operator){
+    const {displayValue} = this.state;
+
+    this.setState({
+      displayValue: '0',
+      operator: operator,
+      operatorIsSet: true,
+      storedValue: displayValue
+    });
+  }
+
+  calculate(){
+    const {displayValue, operator, storedValue } = this.state;
+
+    const operations = {
+      '/': (storedValue, displayValue) => storedValue / displayValue,
+      '*': (storedValue, displayValue) => storedValue * displayValue,
+      '+': (storedValue, displayValue) => storedValue + displayValue,
+      '-': (storedValue, displayValue) => storedValue - displayValue
+    }
+    if(operator){
+      const calculatedValue = operations[operator](storedValue, displayValue);
+
+      this.setState({
+        displayValue: calculatedValue,
+        isDecimal: calculatedValue  % 1 === 0 ? false : true
+      });
+    }
+  }
+
+  clearOperator(){
+    this.setState({
+      displayValue: '0'
+    })
+  }
+
+  changeDecimal(dot){
+    // pull off state then do this
+    const { displayValue, isDecimal } = this.state;
+
+      if(!isDecimal){
+        this.setState({
+          displayValue: displayValue + '.',
+          isDecimal: true
+        });
+      }
+  }
 
   render() {
     const nums = numbers.map((t) => (
-      <div>{t}</div>
+      <button>{t}</button>
     ));
+
+    const calcStyle = {background: '#999999'};
+
     return (
       <div className="App">
-        App Component
-        {nums}
+        <pre>{JSON.stringify(this.state, null, 2)}</pre>
+        <div className="display">
+          {this.state.displayValue}
+        </div>
+        <div className="key-row">
+          <button className="calc-key" onClick={() => {this.inputDigit(7)}}>7</button>
+          <button className="calc-key" onClick={() => {this.inputDigit(8)}}>8</button>
+          <button className="calc-key" onClick={() => {this.inputDigit(9)}}>9</button>
+          <button className="calc-key" onClick={() => {this.clearOperator()}} style={calcStyle}>CE</button>
+        </div>
+        <div className="key-row">
+          <button className="calc-key" onClick={() => {this.inputDigit(4)}}>4</button>
+          <button className="calc-key" onClick={() => {this.inputDigit(5)}}>5</button>
+          <button className="calc-key" onClick={() => {this.inputDigit(6)}}>6</button>
+          <button className="calc-key" onClick={() => {this.inputOperator('/')}} style={calcStyle}>/</button>
+        </div>
+        <div className="key-row">
+          <button className="calc-key" onClick={() => {this.inputDigit(1)}}>1</button>
+          <button className="calc-key" onClick={() => {this.inputDigit(2)}}>2</button>
+          <button className="calc-key" onClick={() => {this.inputDigit(3)}}>3</button>
+          <button className="calc-key" onClick={() => {this.inputOperator('*')}} style={calcStyle}>X</button>
+        </div>
+        <div className="key-row">
+          <button className="calc-key" onClick={() => {this.changeDecimal('.')}}>.</button>
+          <button className="calc-key" onClick={() => {this.inputDigit(0)}}>0</button>
+          <button className="calc-key" onClick={() => {this.calculate()}} >=</button>
+          <button className="calc-key" onClick={() => {this.inputOperator('-')}}style={calcStyle}>-</button>
+        </div>
       </div>
     );
+  }
+
+  onInputChange(clickValue){
+    this.setState({ clickValue });
+  }
+
+  onOneClick(clickValue){
+    this.setState({ clickValue });
   }
 }
 
--- src/components/number_key.js
+++ src/components/number_key.js
@@ -1,9 +1,21 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const NumberKey = props => {
-  return (
-    <button>button</button>
-  );
+class NumberKey extends Component {
+  // construct(props){
+  //   super(props);
+  //   this.state = { clickValue: ""};
+  // }
+
+  render(){
+    return (
+      <button
+
+        >button
+      </button>
+    );
+  }
+
+
 }
 
 export default NumberKey;
